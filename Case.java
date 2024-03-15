abd2e3ae17cfb0e000ae1a1d4ce21d71b3be6055
eliott-guyot <eliott.guyot@etu.univ-orleans.fr>
2024-03-15 10:29:06 +0100

    debut

---
+++ Case.java
@@ -0,0 +1,46 @@
+public class Case {
+    protected boolean contientUneBombe ;
+    protected boolean estDecouverte;
+    protected boolean estMarque;
+
+
+    public Case(){
+        this.contientUneBombe=false;
+        this.estDecouverte=false;
+        this.estMarque=false;
+    }
+
+    public void reset (){
+        this.contientUneBombe=false;
+        this.estDecouverte=false;
+        this.estMarque=false;
+    }
+
+    public void poseBombe(){
+        this.contientUneBombe=true;
+    }
+
+    public boolean contientUneBombe(){
+        return this.contientUneBombe;
+    }
+
+    public boolean estDecouverte(){
+        return this.estDecouverte;
+    }
+
+    public boolean estMarque(){
+        return this.estMarque;
+    }
+
+    public boolean reveler(){
+        if (this.estDecouverte || this.contientUneBombe==false){
+            return true;
+        }
+        return false;
+    }
+
+    public void marquer(){
+        this.estMarque=true;
+    }
+    
+}
---
+++ caseIntelligente.java
@@ -0,0 +1,13 @@
+import java.util.List;
+public class caseIntelligente {
+    private List<Case> lesVoisines;
+
+    public caseIntelligente(){
+        super();
+        
+    }
+
+    public void ajouteVoisine(Case uneCase){
+
+    }
+}
